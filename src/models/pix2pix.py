23ebbbb0b4b4e3096370d47ce2845f3c62c8160b
w1ckedd <alirezachoobineh@ymail.com>
2023-11-01 01:06:29 -0400

    first commit

---
+++ src/models/pix2pix.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class UnetGenerator(nn.Module):
+  def __init__(self):
+    pass
+
+  def forward(self):
+    pass
+
+class PatchGanDiscriminator(nn.Module):
+  def __init__(self):
+    pass
+
+  def forward(self):
+    pass
